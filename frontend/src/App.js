63d329168666fd0c2e1f11ad5f38c97bccfca6dc
Kishan Takoordyal <kishan@kinesis.games>
2020-08-02 17:20:18 +0400

    Add React Part

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+    return(
+        <div className="w-screen h-screen flex justify-center items-center text-2xl tracking-wider font-mono text-white">
+            MERN Stack
+        </div>
+    );
+}
